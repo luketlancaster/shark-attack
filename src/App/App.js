a486d52015f151ba54e3dd545692a9c0d7fd32e0
Luke Lancaster <luketlancaster@gmail.com>
2019-12-11 12:48:12 -0600

    Initial setup for components/data funcs

--- src/App/App.js
+++ src/App/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Shark Attack</h1>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Shark Attack</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/GraveStone/GraveStone.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './GraveStone.scss';
+
+class GraveStone extends React.Component {
+  render() {
+    return (
+      <div>GraveStone</div>
+    );
+  }
+}
+
+export default GraveStone;
