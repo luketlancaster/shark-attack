9bb32950c8cf27d238a883f3b641d2bb8836d5f7
Luke Lancaster <luketlancaster@gmail.com>
2019-12-11 13:11:44 -0600

    Displays live students in an ugly way

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 
+import SharkTank from '../components/SharkTank/SharkTank';
+
+import studentData from '../helpers/data/studentsData';
+
 import './App.scss';
 
 class App extends React.Component {
+  state = {
+    livingStudents: [],
+  }
+
+  componentDidMount() {
+    this.setState({ livingStudents: studentData.livingStudents() });
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Shark Attack</h1>
+        <SharkTank livingStudents={this.state.livingStudents} />
       </div>
     );
   }
--- src/components/SharkTank/SharkTank.js
+++ src/components/SharkTank/SharkTank.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import LiveStudent from '../LiveStudent/LiveStudent';
+import studentShape from '../../helpers/propz/studentShape';
 
 import './SharkTank.scss';
 
 class SharkTank extends React.Component {
+  static propTypes = {
+    livingStudents: PropTypes.arrayOf(studentShape),
+  }
+
   render() {
+    const { livingStudents } = this.props;
+    const studentCards = livingStudents.map((s) => <LiveStudent key={s.id} student={s} />);
+
     return (
-      <div>SharkTank</div>
+      <div className="SharkTank d-flex flex-wrap">
+        <h1>SharkTank</h1>
+        {studentCards}
+      </div>
     );
   }
 }
