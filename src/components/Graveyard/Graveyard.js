5500edfee42ee71881abc36e3a6c6c6a5ab586d9
Luke Lancaster <luketlancaster@gmail.com>
2019-12-12 17:25:39 -0600

    Show dead students

--- src/components/Graveyard/Graveyard.js
+++ src/components/Graveyard/Graveyard.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import GraveStone from '../GraveStone/GraveStone';
+import studentShape from '../../helpers/propz/studentShape';
 
 import './Graveyard.scss';
 
 class Graveyard extends React.Component {
+  static propTypes = {
+    deadStudents: PropTypes.arrayOf(studentShape),
+  }
+
   render() {
+    const { deadStudents } = this.props;
+    const gravestones = deadStudents.map((d) => <GraveStone key={d.id} student={d}/>);
     return (
-      <div>Graveyard</div>
+      <div className="Graveyard">
+        <h2>Graveyard</h2>
+        <div className="d-flex flex-wrap">
+          {gravestones}
+        </div>
+      </div>
     );
   }
 }
