250ab0e87e113c0d07cc45a3296333f9d327eb36
Luke Lancaster <luketlancaster@gmail.com>
2020-02-29 10:33:21 -0600

    Display all alive students in shark tank

--- javascripts/components/tank.js
+++ javascripts/components/tank.js
@@ -2,7 +2,23 @@ import utils from '../helpers/utils.js';
 import personData from '../helpers/data/personData.js';
 
 const tankBuilder = () => {
-  let domString = `Hello ${2 + 4}`;
+  let domString = `
+    <div class="card shark-tank">
+      <div class="card-header">
+        Shark Tank
+      </div>
+      <ul class="list-group list-group-flush">
+  `
+  personData.getAlivePersons().forEach(person => {
+    domString += `
+      <li class="list-group-item">${person.name}</li>
+    `;
+  })
+
+  domString +=`
+      </ul>
+    </div>
+  `;
   utils.printToDom('tank', domString);
 };
 
